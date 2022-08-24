eba9bc3872cf68238c9f2032f50bf49a336d7775
Lucas Cruz Vilaça Rodrigues <lucas.rodrigues@goias.gov.br>
2022-08-24 15:19:13 -0300

    Iframe atualizado

--- src/components/paineis_show/Mineracao.vue
+++ src/components/paineis_show/Mineracao.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
-        <iframe title="Parcelamento 1" width="100%" height="1450px" src="https://app.powerbi.com/view?r=eyJrIjoiZTMxYjc0MWItYjY5Zi00OWNiLThkOGMtMGY3NTZlODEzOWVjIiwidCI6IjY3ZmQ0MzFjLWIyYWQtNDg2Ny04MWJjLWQ3NTYyMjBiNTZkNCJ9&pageName=ReportSection0b19f4c9e1debe840881" frameborder="0" allowFullScreen="true"></iframe>
+        <!--<iframe title="Parcelamento 1" width="100%" height="1450px" src="https://app.powerbi.com/view?r=eyJrIjoiZTMxYjc0MWItYjY5Zi00OWNiLThkOGMtMGY3NTZlODEzOWVjIiwidCI6IjY3ZmQ0MzFjLWIyYWQtNDg2Ny04MWJjLWQ3NTYyMjBiNTZkNCJ9&pageName=ReportSection0b19f4c9e1debe840881" frameborder="0" allowFullScreen="true"></iframe>-->
+        <p>MINERAÇÃO AINDA NÃO POSSUI PAINEL</p>
     </div>
 </template>
 
--- src/components/paineis_show/ProgramasFomento.vue
+++ src/components/paineis_show/ProgramasFomento.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <iframe title="Parcelamento 1" width="100%" height="1450px" src="https://app.powerbi.com/view?r=eyJrIjoiZTMxYjc0MWItYjY5Zi00OWNiLThkOGMtMGY3NTZlODEzOWVjIiwidCI6IjY3ZmQ0MzFjLWIyYWQtNDg2Ny04MWJjLWQ3NTYyMjBiNTZkNCJ9&pageName=ReportSection0b19f4c9e1debe840881" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="Parcelamento 1" width="100%" height="1450px" src="https://app.powerbi.com/view?r=eyJrIjoiMmI3YjY2ZWYtMWEyYi00ZjljLWExOWItNDhkYmRhOGM0NzkyIiwidCI6IjY3ZmQ0MzFjLWIyYWQtNDg2Ny04MWJjLWQ3NTYyMjBiNTZkNCJ9&pageName=ReportSection0b19f4c9e1debe840881" frameborder="0" allowFullScreen="true"></iframe>
     </div>
 </template>
 
